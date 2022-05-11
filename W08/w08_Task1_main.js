d54d80bc56458d5d31e8854b2705849502b53135
搗本有望 <daobenyouwang@tsukimotoaminoMacBook-puro.local>
2022-05-11 20:34:41 +0900

    Initial commit

---
+++ W08/w08_Task1_main.js
@@ -0,0 +1,20 @@
+d3.csv("https://223x213x.github.io/InfoVis2022/W04/w04_task2.csv")
+    .then( data => {
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; });
+
+        var config = {
+            parent: '#drawing_region',
+            width: 256,
+            height: 190,
+            margin: {top:30, right:10, bottom:50, left:60},
+            title: "Sample Data",
+            xlabel: "X label",
+            //ylabel: 'Y label'
+        };
+
+        const bar_plot = new BarChart( config, data );
+        bar_plot.update();
+    })
+    .catch( error => {
+        console.log( error );
+    });
